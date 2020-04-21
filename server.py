4ea85eb0863079dc3fb3c3e5e73dce6da86360be
Levente Fodor <levilevi183@gmail.com>
2020-04-21 17:14:40 +0200

    Formatting documents

--- server.py
+++ server.py
@@ -7,29 +7,16 @@ from bson import json_util, ObjectId, objectid
 
 app = Flask(__name__)
 
-#TODO: refactor this to another file
+# TODO: refactor this to another file
 try:
-    my_conn = DBConnection("virtualspiceapp", "spice", "SpiceAdmin","SpiceAdmin123")
+    my_conn = DBConnection("virtualspiceapp", "spice",
+                           "SpiceAdmin", "SpiceAdmin123")
 except pymongo.errors.OperationFailure:
     print("Authentication error: The Username or Password is not valid")
 
 
-class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
-
-
-
 @app.route("/api/virtualspice")
 def virtualspice():
-    # foods_list = list(my_conn.find_all_items())
-    # return json.dumps(foods_list, default=json_util.default)
-    # return dumps(my_conn.find_all_items())
-    # foods_list = [item for item in my_conn.find_all_items()]
-
-    # return json.dumps(aaaa(foods_list))
     return dumps(my_conn.find_all_items_2())
 
 
@@ -41,43 +28,49 @@ def aaaa(list):
             ret_list.append("almaaa")
     return ret_list
 
+
 @app.route("/api/virtualspice/<name>")
 def get_foods_by_name(name):
     return dumps(my_conn.find_items_by_name(name))
+
 
 @app.route("/api/virtualspice/delete/<id>")
 def delete_foods_by_id(id):
     my_conn.delete_single_item_from_db(id)
     return None
 
+
 @app.route("/api/virtualspice/delete/all")
 def delete_all_foods():
     my_conn.delete_all_items_from_db()
     return None
 
+
 @app.route("/api/getcountofallitems")
 def get_number_of_all_items():
     return jsonify(my_conn.get_count_of_all_items())
-    
+
 
 @app.route("/api/test")
 def testestest():
     return jsonify(my_conn.counts_per_type())
 
+
 @app.route("/api/shoppinglist")
 def shoppinglist():
     return "shoppinglist"
+
 
 @app.route("/api/recepies")
 def recepies():
     return "recepies"
 
+
 @app.route("/api/login")
 def login():
     return "login"
 
+
 if __name__ == "__main__":
     app.run(port=5000)
-    
-
 
